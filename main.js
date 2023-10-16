680cb44b37440fda8ac5b0c3cebaa3cdf0abd7be
lebecasa <148072673+lebecasa@users.noreply.github.com>
2023-10-16 10:10:41 -0300

    att

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function idElementoAudio() {
+    document.querySelector('#idElementoAudio').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSom;
+
+let contador=0;listadeteclas
+
+while(contador<9){
+    listadeteclas[contador].onclick
+    contador=contador+1
+}
+
+const listadeteclas=document.querySelectorAll('.tecla');
